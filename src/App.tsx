b499c91ef6dc25ab0529214b7890c2fc5f04b199
shahin <shahinahd3@gamil.com>
2024-03-12 09:44:00 +0530

    Form Step JSX/CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import './App.css'
+import { useMultistepFrom } from "./useMultistepFrom";
 
 function App() {
-
+    const {steps, currentStepIndex,step, isFirstStep,back,next,isLastStep} = useMultistepFrom([])
   return (
-    <>
-     
-    </>
-  )
+    <div
+      style={{
+        position: "relative",
+        background: "white",
+        border: "1px solid black",
+        padding: " 2rem",
+        margin: "1rem",
+        borderRadius: ".5rem",
+        fontFamily: "Arial",
+      }}
+    >
+      <form action="">
+        <div
+          style={{
+            position: "absolute",
+            top: ".5rem",
+            right: ".5rem",
+          }}
+        >
+          {currentStepIndex +1}/ {steps.length}
+        </div>
+        {step}
+        <div style={{
+          marginTop:"1rem",
+          display:"flex",
+          gap:".5rem",
+          justifyContent: "flex-end"
+        }} >
+          {!isFirstStep && <button onClick={back} type="button">Back</button>}
+          <button type="button" onClick={next} >{isLastStep ? "Finish":"Next"}</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/useMultistepFrom.ts
+++ src/useMultistepFrom.ts
@@ -24,6 +24,9 @@ export function useMultistepFrom(steps: ReactElement[]){
     return {
         currentStepIndex,
         step : steps[currentStepIndex],
+        steps,
+        isFirstStep: currentStepIndex !== 0,
+        isLastStep: currentStepIndex === steps.length -1,
         goTo,
         next,
         back
